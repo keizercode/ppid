f4c3c41ce00ce83188bf57b6eed3ea177173d6cd
keizercode <luqman.arohim@if.uai.ac.id>
2026-04-05 23:15:18 +0700

    update surat izin

--- obj/Debug/net8.0/PermintaanData.AssemblyInfo.cs
+++ obj/Debug/net8.0/PermintaanData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d18b5b609f59df93d23ae0611a6fb0ebec20f989")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f72aab20de22ed8586a97e7444d4a763f4824226")]
 [assembly: System.Reflection.AssemblyProductAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
