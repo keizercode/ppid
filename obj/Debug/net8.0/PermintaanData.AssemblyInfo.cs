79e331de9438fe4bd399fc0b42f78f1ca59a6578
keizercode <luqman.arohim@if.uai.ac.id>
2026-04-08 22:11:03 +0700

    update layout

--- obj/Debug/net8.0/PermintaanData.AssemblyInfo.cs
+++ obj/Debug/net8.0/PermintaanData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cb90f6210768f1ba019ec797a255745d8d61b29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a0772d7c26e487cc682373da393f42a00f62239")]
 [assembly: System.Reflection.AssemblyProductAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
