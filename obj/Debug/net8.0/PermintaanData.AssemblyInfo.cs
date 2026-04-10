7970b1df4f6d539c25503fd4b077a09921c5df3f
keizercode <luqman.arohim@if.uai.ac.id>
2026-04-10 10:57:36 +0700

    update index home

--- obj/Debug/net8.0/PermintaanData.AssemblyInfo.cs
+++ obj/Debug/net8.0/PermintaanData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a92be7429117c8c433a70e73cf886964957feeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+343b77f6da073e773668c3e7d729d214ad9b1f5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
