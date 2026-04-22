c984e99e30dc8db984b4896777c1cc7be6929e1b
keizercode <luqman.arohim@if.uai.ac.id>
2026-04-22 11:30:05 +0700

    update phone and fix ui

--- obj/Debug/net8.0/PermintaanData.AssemblyInfo.cs
+++ obj/Debug/net8.0/PermintaanData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9e8575119d07b94d2430ae36aef1e13b34d202d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6564e9a99c92cacf22f56d39592037b2b446a015")]
 [assembly: System.Reflection.AssemblyProductAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
