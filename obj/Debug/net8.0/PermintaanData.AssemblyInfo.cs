0a402bf61f7d2ef8cbf1e180ae55c7ea798c8dac
keizercode <luqman.arohim@if.uai.ac.id>
2026-04-28 20:54:54 +0700

    update reschedulesubtask

--- obj/Debug/net8.0/PermintaanData.AssemblyInfo.cs
+++ obj/Debug/net8.0/PermintaanData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d4daac2e1df07cea4619e1b2a20e495420f2b4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ff96c9619e735d13175645e14079b7e0867fd05")]
 [assembly: System.Reflection.AssemblyProductAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
