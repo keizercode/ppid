b8c01749ce86d262e992c00df875396ae2c74a06
keizercode <luqman.arohim@if.uai.ac.id>
2026-05-02 23:02:18 +0700

    rapihkan table tiap halaman

--- obj/Debug/net8.0/PermintaanData.AssemblyInfo.cs
+++ obj/Debug/net8.0/PermintaanData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3bd8ddba6e9f997420f9c8c4f773fd5777df1a21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e15f2660353595146b04a49c51fb8c2421bf2dc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
