6109d86bb0bb593e4c906e14ea459b26ace5527f
keizercode <luqman.arohim@if.uai.ac.id>
2026-05-04 15:48:39 +0700

    setup tailwind cli 4 bukan cdn

--- obj/Debug/net8.0/PermintaanData.AssemblyInfo.cs
+++ obj/Debug/net8.0/PermintaanData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ade0d2ce3fdeaee49faa589cfef7396faf665d16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd17367fd894c25166ac64c94f608e7db96c904a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
