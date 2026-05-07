456ddaf1257154eddb7100c7308600f5c08c8c19
keizercode <luqman.arohim@if.uai.ac.id>
2026-05-07 11:19:32 +0700

    add file preview modal to subtask petugasloket and kasubkelkdi, and hasil feedback

--- obj/Debug/net8.0/PermintaanData.AssemblyInfo.cs
+++ obj/Debug/net8.0/PermintaanData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5271e4c05244e26abb3cb483df1e9049784f677")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d52d12100661504f1176dff396aeffda19929fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
