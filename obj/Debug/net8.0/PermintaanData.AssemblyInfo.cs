d71c2c7c8e52d9e5051b37ae8ef771cb529b51ed
keizercode <luqman.arohim@if.uai.ac.id>
2026-05-08 00:48:00 +0700

    fix

--- obj/Debug/net8.0/PermintaanData.AssemblyInfo.cs
+++ obj/Debug/net8.0/PermintaanData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94895c7dd09d1863909a2be12c544ff6769e5510")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88d00aeb526e7b96fe17158a84db00870af8a26f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
