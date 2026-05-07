52516d11fe035f692a3fc77481d33a0f7c2b4825
keizercode <luqman.arohim@if.uai.ac.id>
2026-05-07 11:54:28 +0700

    update subtask petugas loket

--- obj/Debug/net8.0/PermintaanData.AssemblyInfo.cs
+++ obj/Debug/net8.0/PermintaanData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d52d12100661504f1176dff396aeffda19929fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c7fa85d4476446b252473a60a983813e0e6ac8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PermintaanData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
